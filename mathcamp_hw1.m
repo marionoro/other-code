4e6f905adfa8fdaf6888ba67b74e72382e2ddd59
marionoro <54335685+marionoro@users.noreply.github.com>
2020-10-01 10:59:03 -0500

    Update mathcamp_hw1.m

--- mathcamp_hw1.m
+++ mathcamp_hw1.m
@@ -1,33 +1,21 @@
-%5.2
-
-y=0
-T = 10
-y_t = y
-for i in 1:T
-    
-    
-    
-clear; clc;
 % 2
 t=1000; %size (+1) 
 mp = [1 0 -1];
-N=5000;
+N=5;
 Y=zeros(t,N);
 i=1;
 y0=zeros(t,1);
    while i<=N;
        for j=2:t
-           e = mp((rand(t,1)<1/3) + (rand(t,1)<1/3)+1);
+           e = mp((rand(t,1)<1/3) + (rand(t,1)<2/3)+1);
            y0(j,1)=y0(j-1,1)+e(j);
        end
        Y(:,i)=y0;
        y0=zeros(t,1);
        i=i+1;
    end
-%   plot(Y(:,[1 2 3 4 5]));
-   
-   plot(Y(:,[2 15 125 654 1543 2054 2765 3251 3988 4311]));
-    
+   plot(Y(:,[1 2 3 4 5]));
+     
 %Markov Chains
 % 1
 p = 0.3
@@ -51,9 +39,6 @@ pi_1000 = pi_0 * transitionMatrix^1000;
 pi_1000
 
 % 3
-bar(1:length(pi_1000),pi_1000)
-
-% I don't know if this is right
 epsilon_mean = p - q
 epsilon_var = p^2 + p - 2 * p * q + q + q^2
     
@@ -67,8 +52,6 @@ hold on
 plot([0:1:200], normpdf([0:1:200],theory_mean,sqrt(theory_var)))
 
 hold off
-
-plot([-80:1:80], normpdf([-80:1:80],0,sqrt(1000 * 2/3)))
 
 % 5.2
 % 
@@ -110,8 +93,3 @@ A = max(log(intermed_matrix) + beta.*ones(N,1)*V0,[],1)'
 
 max(w_grid' .* (A == max(A)))
 % ^ This returns the steady state of capital
-
-
-
-
-
